d215f5c0f3f887efba87c22b1c3855ca53e046dc
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-10-01 18:24:07 +0100

    #1 test createUser

--- src/main/java/lab/tracer/App.java
+++ src/main/java/lab/tracer/App.java
@@ -1,12 +1,21 @@
 package lab.tracer;
 
 import lab.tracer.config.AppConfig;
+import lab.tracer.entity.User;
+import lab.tracer.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
         try (var ctx = new AnnotationConfigApplicationContext(AppConfig.class)) {
             System.out.println("Spring Transaction Tracer started with " + ctx.getBeanDefinitionCount() + " beans");
+            User user = new User();
+            user.setName("Znaji");
+            user.setEmail("Znaji@Email.com");
+
+            UserService userService = ctx.getBean(UserService.class);
+            userService.createUser(user);
+            System.out.println("End");
         }
     }
 }
